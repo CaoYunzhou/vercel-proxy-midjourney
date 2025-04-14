e0079b02c4a8716377d8241523efd20aca06f9be
caoyunzhou <28099773+CaoYunzhou@users.noreply.github.com>
2025-04-14 17:39:08 +0800

    fix

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-  const target = 'https://cdn.discordapp.com';
+  const target = 'https://media.discordapp.net';
 
   // 创建代理并转发所有请求
   createProxyMiddleware({
